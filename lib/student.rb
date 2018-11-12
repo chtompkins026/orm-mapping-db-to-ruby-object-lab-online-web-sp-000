7037c09484c909dd3487d2800f4096bb157061b3
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-12 18:47:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   
   
   def self.new_from_db(row)
-    new_song = self.new  # self.new is the same as running Song.new
+    new_student = self.new  # self.new is the same as running Song.new
     new_song.id = row[0]
     new_song.name =  row[1]
     new_song.length = row[2]
